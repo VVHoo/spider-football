ece64586df84b9cb408478656a5fe82b4e8b1d54
Hoo <136200456@qq.com>
2019-11-22 15:57:40 +0800

    feat: 获取cookie逻辑

--- task/get-data.js
+++ task/get-data.js
@@ -46,9 +46,19 @@ let calculateLine = async (info) => {
   }
 }
 async function getData () {
-  console.log('begin spider')
-  puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] }).then(async browser => {
+  puppeteer.launch({ headless: false, args: ['--no-sandbox', '--disable-setuid-sandbox'] }).then(async browser => {
     try {
+      const getCookiePage = await browser.newPage()
+      await getCookiePage.goto(url, {
+        waitUntil: 'networkidle0'
+      });
+      await getCookiePage.click('.more2 > div:last-child')
+      await getCookiePage.click('#ul_filter li:nth-child(2)')
+      await getCookiePage.click('#selectAllBtn')
+      await getCookiePage.click('#filterBar > .btn')
+      const cookies = await getCookiePage.cookies()
+      const filterType = cookies.filter(item => item.name === 'filterType')[0].value
+      await getCookiePage.close()
       const page = await browser.newPage()
       await page.setCookie({
         "domain": "m.win007.com",
@@ -57,7 +67,7 @@ async function getData () {
         "path": "/",
         "sameSite": "unspecified",
         "storeId": "0",
-        "value": "0!31!8!11!60!60!60!60!37!9!9!12!12!12!12!12!12!12!12!5!10!17!17!17!17!17!17!17!17!17!17!7!6!6!119!137!30!70!273!279!279!279!292!292!292!415!415!140!40!33!693!1416!1416!1416!1416!203!203!203!203!203!203!203!203!203!1421!573!573!573!138!128!128!128!128!128!121!121!165!221!221!221!221!129!290!290!170!124!358!358!358!217!135!130!133!131!240!240!240!240!240!315!321!321!740!740!740!332!332!332!332!341!354!354!391!450!450!450!469!533!613!745!423!41!587!587!722!722!950!950!950!950!950!950!950!950!950!677!677!837!837!837!837!837!1106!476!476!476!896!896!915!927!927!967!967!969!1000!1000!729!729!1062!1062!1062!1062!1062!1062!1405!1405!1116!1122!1122!399!399!1138!1138!1160!1169!1227!1227!1272!1272!1272!1411!1412!1457!1461!1466!1466!1479!1514!1514!1533!1533!1539!1558!1558!1558!1558!1558!1558!1558!1558!1558!1558!1558!1558!1558!1562!1562!1567!1583!1583!1583!1583!1583!1583!1593!1593!1599!1599!1604!1609!1612!1620!1620!1620!1620!1620!1620!1620!1620!1620!1623!1623!1634!1634!1634!1634!1634!1634!1634!1634!1634!1634!1641!1641!1641!1641!1641!1641!1641!1641!1641!1641!1641!1641!1641!1641!1641!1641!1641!1641!1641!1641!1655!1655!1682!1682!1683!1729!1750!1768!1817!1817!1820!1834!1834!1834!1834!1834!1840!1840!1840!1840!1866!1866!1957!2045!2045!2045!2051!2051!2051!2066!2066!2066!2066!2070!2070!2070!2070!2070!2074!2074!441",
+        "value": filterType,
         "id": '3'
       })
       await page.goto(url, {
@@ -88,7 +98,6 @@ async function getData () {
       await browser.close()
     } catch (e) {
       console.log(e)
-      await browser.close()
     }
   })
 }

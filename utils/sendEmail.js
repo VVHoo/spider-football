762b2c2d5714ab2fa0f5f7496ec8aa3356c20b8a
VVHoo <vvhoo@vvhoo-deMBP.lan>
2021-06-20 15:34:36 +0800

    feat: 增加邮箱

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -10,7 +10,7 @@
 // 发送邮件逻辑
 const nodemailer = require("nodemailer");
 // 创建一个SMTP客户端配置
-const receivers = ["770822273@qq.com", "136200456@qq.com"];
+const receivers = ["770822273@qq.com", "215372541@qq.com", "136200456@qq.com"];
 const emailConfig = {
   host: "smtp.qq.com",
   port: 465,
